bdaf1c091dce0973118b91f4f9aa749e3a179034
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-12 03:53:13 +0100

    Fixed overlapping issues in hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,23 +25,25 @@ function Hero() {
   }, []);
 
   return (
-    <section className="-z-50 bg-home-hero bg-cover bg-no-repeat bg-bottom h-screen w-full flex items-end justify-center relative hero">
-      <div className="text-grad w-full h-screen flex flex-col items-center justify-center text-center px-5 xl:px-[260px] 2xl:px-[302px]">
-        <HeroNav />
-        <div className=" mt-auto pb-[20%] sm:pb-[10%]">
-          <h1 className="font-display text-white text-2xl leading-tight xl:text-3xl 2xl:text-4xl">
-            Welkom bij Denise Anna
-          </h1>
-          <p className="text-white text-sm font-light mb-11 xl:text-base">
-            Velit qui voluptate pariatur minim cillum quis adipisicing ullamco
-            irure ex aliqua et. Culpa Lorem laborum reprehenderit ea qui elit
-            cupidatat duis laborum. Dolore sint labore quis id minim aute
-            occaecat Lorem sit reprehenderit nulla velit.
-          </p>
-          <Button>Aanmelden</Button>
+    <>
+      <HeroNav />
+      <section className="-z-50 bg-home-hero bg-cover bg-no-repeat bg-bottom h-screen w-full flex items-end justify-center relative hero">
+        <div className="text-grad w-full h-screen flex flex-col items-center justify-center text-center px-5 xl:px-[260px] 2xl:px-[302px]">
+          <div className="mt-auto pb-[20%] sm:pb-[10%]">
+            <h1 className="font-display text-white text-2xl leading-tight xl:text-3xl 2xl:text-4xl">
+              Welkom bij Denise Anna
+            </h1>
+            <p className="text-white text-sm font-light mb-11 xl:text-base">
+              Velit qui voluptate pariatur minim cillum quis adipisicing ullamco
+              irure ex aliqua et. Culpa Lorem laborum reprehenderit ea qui elit
+              cupidatat duis laborum. Dolore sint labore quis id minim aute
+              occaecat Lorem sit reprehenderit nulla velit.
+            </p>
+            <Button>Aanmelden</Button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
