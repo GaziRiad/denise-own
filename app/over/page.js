18811aa7a9ccf5528a577c5bbb0f3e93249c0be3
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-13 10:06:45 +0100

    added noise layer to all website

--- app/over/page.js
+++ app/over/page.js
@@ -2,11 +2,13 @@ import AccordionSection from "@/components/AccordionSection";
 import Footer from "@/components/Footer";
 import HeroOver from "@/components/HeroOver";
 import MobileSection from "@/components/MobileSection";
+import NoiseLayer from "@/components/NoiseLayer";
 import OverBlocksSection from "@/components/OverBlocksSection";
 
 function page() {
   return (
     <main>
+      <NoiseLayer />
       <HeroOver />
       <OverBlocksSection />
       <MobileSection />
