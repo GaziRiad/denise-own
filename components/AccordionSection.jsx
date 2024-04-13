2783f7ca858d7b3272b8c79bb02dba680cb5b9f0
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-13 08:34:17 +0100

    deleted x padding

--- components/AccordionSection.jsx
+++ components/AccordionSection.jsx
@@ -4,7 +4,7 @@ import AccordionItem from "./AccordionItem";
 function AccordionSection() {
   return (
     <section className="min-h-[70vh] flex items-center justify-center bg-brownLight py-24 px-5 lg:px-10 3xl:py-44">
-      <div className="container mx-auto flex-col flex justify-between gap-6 lg:px-10 xl:items-start xl:flex-row xl:max-w-6xl 2xl:max-w-[1400px]">
+      <div className="container mx-auto flex-col flex justify-between gap-6 xl:items-start xl:flex-row xl:max-w-6xl 2xl:max-w-[1400px]">
         <h1 className="flex-1 text-brown font-display text-xl xl:-mt-16 xl:text-2xl">
           Veel gestelde vragen
         </h1>
