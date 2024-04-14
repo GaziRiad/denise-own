b1d9a38df06472a20d9cdff116548e916f1e8ff6
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-14 12:20:05 +0100

    added google tag to website

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Script from "next/script";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+import GoogleTagManager from "@/components/custom/GoogleTagManager";
 
 export const metadata = {
   title: "Home",
@@ -17,27 +18,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={figtree.className}>
       <head>
-        <Script id="google-tag">
-          {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TSXVZ2TP');
-          `}
-        </Script>
+        <GoogleTagManager containerId="GTM-TSXVZ2TP" />
       </head>
-      <body className="text-brown">
-        {/* <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TSXVZ2TP"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript> */}
-        {children}
-      </body>
+      <body className="text-brown">{children}</body>
     </html>
   );
 }
