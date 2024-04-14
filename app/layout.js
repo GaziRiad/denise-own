d81fc023634635253d605c5cdeb3d35615ae26e2
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-14 12:06:52 +0100

    added google tag to website

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Figtree } from "next/font/google";
 import "./globals.css";
 
@@ -15,7 +16,28 @@ const figtree = Figtree({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={figtree.className}>
-      <body className="text-brown">{children}</body>
+      <head>
+        <Script id="google-tag">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TSXVZ2TP');
+          `}
+        </Script>
+      </head>
+      <body className="text-brown">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSXVZ2TP"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
