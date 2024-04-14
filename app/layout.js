a68333ad1ad455f722d596839cb9b637dc41c63b
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-15 00:25:20 +0100

    Linked cookiebot wit gtm

--- app/layout.js
+++ app/layout.js
@@ -18,14 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={figtree.className}>
       <head>
-        <Script
-          id="Cookiebot"
-          src="https://consent.cookiebot.com/uc.js"
-          data-cbid="7fc4ac26-78f5-40a6-b0f9-d3d55af8437b"
-          // data-blockingmode="auto"
-          data-blockingmode="manual"
-          type="text/javascript"
-        ></Script>
+        <Script id="Cookiebot"></Script>
         <GoogleTagManager containerId="GTM-TSXVZ2TP" />
       </head>
       <body className="text-brown">{children}</body>
